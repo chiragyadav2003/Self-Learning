ebac4c6fd0cfd13c16d386070841f541e8d71635
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-15 23:19:13 +0530

    adding post route for video info

--- hono_mongodb_bun/src/learn.js
+++ hono_mongodb_bun/src/learn.js
@@ -9,4 +9,16 @@ app.get("/", (c) => {
     return c.html("<h1>Welcome to hono backend</h1>")
 })
 
+app.post("/video", async (c) => {
+    const { videoName, channelName, duration } = await c.req.json();
+    const newVideo = {
+        id: uuidv4(),
+        videoName,
+        channelName,
+        duration
+    }
+    videos.push(newVideo);
+    return c.json(newVideo);
+})
+
 export default app;
